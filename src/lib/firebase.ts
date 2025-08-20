0f96dbe352ddce57c84fea2efec547d402c6eb08
Patrick Craven <cravenwspatrick@gmail.com>
2025-08-20 06:32:46 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,17 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDnVcmt5cTjPXz-_d8tSAzvBr07vIKdV20",
-  authDomain: "shyft-xphli.firebaseapp.com",
-  databaseURL: "https://shyft-xphli-default-rtdb.firebaseio.com",
-  projectId: "shyft-xphli",
-  storageBucket: "shyft-xphli.appspot.com",
-  messagingSenderId: "182080370256",
-  appId: "1:182080370256:web:92042e2eb8a90064228355"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
